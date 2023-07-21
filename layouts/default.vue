6d2755f6b516d7762f3f88362460bd05d8a178ba
Ali Karam <ak@alikaram.com>
2023-07-21 16:35:59 +0300

    feat: :sparkles: add default layout and navbar

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+// import Navbar1 from '..Navbar1.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <Navbar1 />
+        <slot />
+    </div>
+</template>
+
+<style scoped></style>
