cd4523d38cd6566c4a20dc39824b5a0e54667ecb
Ali Karam <ak@alikaram.com>
2023-07-21 16:34:41 +0300

    feat: :heavy_plus_sign: add tailwind nuxt module

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+// import TailwindTest from '~/components/TailwindTest.vue';
+</script>
+
+<template>
+    <div>
+        <TailwindTest />
+    </div>
+</template>
+
+<style scoped></style>
